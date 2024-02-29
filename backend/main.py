d1b2060d661989325dd57ee3a24404802a7ca111
aufarhmn <aufa.nas2003@mail.ugm.ac.id>
2024-02-29 17:36:17 +0700

    fix: reorganize backend

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,24 @@
-from app import app
+from flask import Flask 
+from mongoengine import connect
+import os 
+
+app = Flask(__name__) 
+
+from dotenv import load_dotenv
+load_dotenv()
+
+if os.getenv('MONGODB_URI') is None or os.getenv('MONGODB_DB') is None:
+    raise ValueError("MongoDB is not properly configured!")
+
+app.config['MONGODB_SETTINGS'] = {
+    'db': os.getenv('MONGODB_DB'),
+    'host': os.getenv('MONGODB_URI'),
+}
+connect(**app.config['MONGODB_SETTINGS'])
+
+@app.route('/')
+def index():
+    return "Hello from Wellnes Whale!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
