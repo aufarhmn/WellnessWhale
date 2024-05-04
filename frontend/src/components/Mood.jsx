7a877e31c645a0b1e4811647d79caf97b00bb388
aufarhmn <aufa.nas2003@mail.ugm.ac.id>
2024-05-04 22:27:30 +0700

    feat: input mood page (underway)

---
+++ frontend/src/components/Mood.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Mood() {
+  return (
+    <>
+      <div className='w-full h-[950px] bg-green-100'>
+
+      </div>
+    </>
+  )
+}
---
+++ frontend/src/pages/mood/input.jsx
@@ -0,0 +1,13 @@
+import Mood from '../../components/Mood'
+import Navbar from '../../components/Navbar'
+import Footer from '../../components/Footer'
+
+export default function InputMood() {
+  return (
+    <>
+      <Navbar />
+      <Mood />
+      <Footer />
+    </>
+  )
+}
