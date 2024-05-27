fe136f219506d7327ff93d3de5c490643a849272
aufarhmn <aufa.nas2003@mail.ugm.ac.id>
2024-05-27 07:20:25 +0700

    fix: href to article

--- frontend/src/components/Interactive.jsx
+++ frontend/src/components/Interactive.jsx
@@ -18,38 +18,42 @@ export default function Interactive() {
         </div>
         <div className="flex flex-col lg:flex-row w-full px-6 lg:px-20 py-2">
           <div className="w-full lg:w-1/2 lg:mx-4 flex flex-col justify-center items-center mb-4 lg:mb-0">
-            <div className="w-full h-[300px] lg:h-[250px] bg-white border-4 border-green-400 rounded-[18px] flex flex-row justify-between items-center">
-              <div className="text-center px-12 lg:text-left w-[70%]">
-                <p className="text-lg lg:text-[32px] font-semibold my-2">
-                  Relaksasi dengan Air Beras
-                </p>
+            <Link href="/article/1">
+              <div className="w-full h-[300px] lg:h-[250px] bg-white border-4 border-green-400 rounded-[18px] flex flex-row justify-between items-center">
+                <div className="text-center px-12 lg:text-left w-[70%]">
+                  <p className="text-lg lg:text-[32px] font-semibold my-2">
+                    Relaksasi dengan Air Beras
+                  </p>
+                </div>
+                <div className="w-[30%]">
+                  <Image
+                    src={Beras}
+                    className="aspect-square"
+                    width={180}
+                    height={180}
+                  />
+                </div>
               </div>
-              <div className="w-[30%]">
-                <Image
-                  src={Beras}
-                  className="aspect-square"
-                  width={180}
-                  height={180}
-                />
-              </div>
-            </div>
+            </Link>
           </div>
           <div className="w-full lg:w-1/2 lg:mx-4 flex flex-col justify-center items-center">
-            <div className="w-full h-[300px] lg:h-[250px] bg-white border-4 border-green-400 rounded-[18px] flex flex-row justify-between items-center">
-              <div className="text-center px-12 lg:text-left w-[70%]">
-                <p className="text-lg lg:text-[32px] font-semibold my-2">
-                  Relaksasi latihan soal
-                </p>
+            <Link href="/article/2">
+              <div className="w-full h-[300px] lg:h-[250px] bg-white border-4 border-green-400 rounded-[18px] flex flex-row justify-between items-center">
+                <div className="text-center px-12 lg:text-left w-[70%]">
+                  <p className="text-lg lg:text-[32px] font-semibold my-2">
+                    Meditasi dengan Air Hujan
+                  </p>
+                </div>
+                <div className="w-[30%]">
+                  <Image
+                    src={Latsol}
+                    className="aspect-square"
+                    width={180}
+                    height={180}
+                  />
+                </div>
               </div>
-              <div className="w-[30%]">
-                <Image
-                  src={Latsol}
-                  className="aspect-square"
-                  width={180}
-                  height={180}
-                />
-              </div>
-            </div>
+            </Link>
           </div>
         </div>
       </div>
